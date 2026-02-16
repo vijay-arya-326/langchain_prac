7deebb9e57f6b1125218db897c3e1fdf0a612405
Vijay Kumar <vijayarya326@gmail.com>
2026-02-16 13:47:05 +0530

    updated files

--- lib.py
+++ lib.py
@@ -1,3 +1,5 @@
+import sys
+
 from langchain_core.messages import HumanMessage, AIMessage
 from langgraph.graph import StateGraph
 import traceback
@@ -9,23 +11,37 @@ def displayAllMessages(response):
     for m in response['messages']:
         print(m.pretty_print())
 
+def Logmsg(msg:str):
+    print(msg, file=sys.stderr)
 
-def cliBot(graphInstance: StateGraph, config=None):
+def cliBot(graphInstance: StateGraph, config=None, show_all_msg = False):
+    """
+
+    :param graphInstance: StateGraph Instance
+    :param config: Memory Configration
+    :param show_all_msg: Show all messages or last AI message
+    :return:
+    """
     try:
-        exitPrases = ["exit", "bye", "tata"]
+        exit_phrases = ["exit", "bye", "tata"]
         flag = True
         while flag:
             inputMsg = input("Please enter your message: ")
-            if inputMsg.strip().lower() in exitPrases:
+            if inputMsg.strip().lower() in exit_phrases:
                 flag = False
                 print("Human requested to close session!!! Bye see you around")
             else:
                 strippedMsg = inputMsg.strip()
                 humanMessage = [HumanMessage(content=strippedMsg)]
-                r = graphInstance.invoke({"messages": humanMessage})
-                print(f"Question:\n {strippedMsg}", end ="\n==============\n")
-                print("Assistant:\n")
-                print(getAIMessage(response=r), end ="\n=============\n")
+                r = graphInstance.invoke({"messages": humanMessage}, config)
+                if show_all_msg == False:
+                    print(f"Question:\n {strippedMsg}", end ="\n==============\n")
+                    print("Assistant:\n")
+                    print(getAIMessage(response=r), end ="\n=============\n")
+                else:
+                    displayAllMessages(r)
     except Exception as e:
         print(e)
         print(traceback.format_exc())
+
+
